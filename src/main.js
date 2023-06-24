227e3ffea4f9e6f99254e108427a1d537718d092
fangtasyjx <jinxinf@mtu.edu>
2023-06-24 21:06:44 +0800

    去掉App引用

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
+// import App from './App.vue'
 import router from "./route/index"; 
 import { createPinia } from 'pinia' 
 
